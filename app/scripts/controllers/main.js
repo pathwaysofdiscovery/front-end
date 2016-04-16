a5b5cc9851c26621a88892be8ca9ed50538cba52
Grayson Hilliard <wghilliard@cmdcenter.local>
2016-04-16 16:50:15 -0500

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name podsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the podsApp
+ */
+angular.module('podsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
